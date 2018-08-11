ad9eac7e67f6b3490b255753b54ceea60244d487
harithapriyavemuri <41899147+harithapriyavemuri@users.noreply.github.com>
2018-08-11 12:33:49 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import java.util.Scanner;
+
+class Ideone
+{
+  public static void main(String args[]) throws java.lang.Exception
+  {
+   int number, i, sum = 0;
+   Scanner sc = new Scanner(System.in);
+   number = sc.nextInt();	
+  for(i = 1;i <= number; i++)
+  {
+	sum = sum + i; 
+   }	
+   System.out.println(sum);
+}
+}
