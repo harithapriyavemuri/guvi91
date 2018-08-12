f91df654f26f2fb3fc903824eb6d02b401719582
harithapriyavemuri <41899147+harithapriyavemuri@users.noreply.github.com>
2018-08-12 12:24:37 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,34 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class pali
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int i,c=0;
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	for(i=1;i<=n;i++)
+	{
+		if(n% i ==0)
+		{
+			c++;
+			
+		}
+	}
+	        if(c==2)
+	        {
+	        	System.out.println("yes");
+	        }
+	        else
+	        {
+	        	 System.out.println("no");
+	        }
+	}
+}
+
+	        
+	        
