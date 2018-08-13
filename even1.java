85fed09401dbf7df9eda1ae94c37d29bfacc78df
harithapriyavemuri <41899147+harithapriyavemuri@users.noreply.github.com>
2018-08-13 12:33:48 +0530

    Create even1.java

---
+++ even1.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i;
+		Scanner sc=new Scanner(System.in);
+		int min=sc.nextInt();
+		int max=sc.nextInt();
+		     for(i=min;i<=max;i++){
+		     	if(i%2==0)
+		     	System.out.println(i);
+		     	
+		     }
+	}
+}
