8fd572792be4e2e17f74fec743d2c15da2c1536f
harithapriyavemuri <41899147+harithapriyavemuri@users.noreply.github.com>
2018-08-14 13:10:19 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,24 @@
+rt java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter a number");
+			int n=sc.nextInt();
+		       if(n%4==0)
+			{
+				System.out.println("yes");
+			}
+			else
+			{
+				System.out.println("no");
+			}
+		}
+	}
+}
