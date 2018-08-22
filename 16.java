385f4ded8dbfab557faba450d74b385007118db2
harithapriyavemuri <41899147+harithapriyavemuri@users.noreply.github.com>
2018-08-22 10:57:41 +0530

    Update 16.java

--- 16.java
+++ 16.java
@@ -1,10 +1,10 @@
+import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
-import java.util.*;
 	public static void main (String[] args) throws java.lang.Exception
 	{
        int low = 20, high = 50;
